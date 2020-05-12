d56e3091c6c4145c927f3285b9f3096f22b9bf08
Radek Szumski <usa.rs@live.com>
2020-05-12 18:32:38 +0100

    ADD configuration for environments, HTTPS and the first route

---
+++ config.js
@@ -0,0 +1,34 @@
+/*
+*
+* Create and export configuration variables
+*
+* in PowerShell you start it as:
+* PS C:\path\to\your\project> $env:NODE_ENV="production";node index.js
+* in Linux:
+* some-user@some-linux path/to/your/project $ NODE_ENV=production node index.js
+*
+*/
+
+// container for all the environments
+const environments = {};
+
+// Staging environment (default)
+environments.staging = {
+    'httpPort': 3000,
+    'httpsPort': 3001,
+    'envName': 'staging'
+};
+
+// Production environment
+environments.production = {
+    'httpPort': 5000,
+    'httpsPort': 5001,
+    'envName': 'production'
+};
+
+// Determine which environment was passed as a command-line argument
+const currentEnvironment = typeof (process.env.NODE_ENV) === 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+const environmentToExport = environments[currentEnvironment] || environments.staging;
+
+module.exports = environmentToExport;
--- index.js
+++ index.js
@@ -6,13 +6,19 @@
 
 // Dependencies
 const http = require('http');
-const url = require('url');
+const https = require('https');
+const fs = require('fs');
 const { StringDecoder } = require('string_decoder');
+const config = require('./config');
 
 const handlers = {};
 
+handlers.ping = (data, callback) => {
+    callback(200);
+};
+
 handlers.sample = (data, callback) => {
-    callback(406, {'name': 'sample handler'})
+    callback(200, data);
 };
 
 handlers.notFound = (data, callback) => {
@@ -20,17 +26,19 @@ handlers.notFound = (data, callback) => {
 };
 
 const router = {
-    'sample': handlers.sample
+    ping: handlers.ping,
+    sample: handlers.sample
 };
 
-const server = http.createServer((req, res) => {
-    const parsedURL = url.parse(req.url, true);
+const unifiedServer = (req, res) => {
+    // const parsedURL = url.parse(req.url, true);
+    const parsedURL = new URL(req.url, 'http://localhost');
     const path = parsedURL.pathname;
     const trimmedPath = path.replace(/^\/+|\/+$/g, '');
     const method = req.method.toLowerCase();
-    const queryStringObject = parsedURL.query;
+    const queryStringObject = parsedURL.searchParams;
     const headers = req.headers;
-    
+
     //Get the payload if any
     const decoder = new StringDecoder('utf-8');
     let buffer = '';
@@ -62,8 +70,21 @@ const server = http.createServer((req, res) => {
             console.log(`Returning this response: ${statusCode}, ${payloadString}`);
         });
     });
+};
 
-
+const httpServer = http.createServer((req, res) => {
+    unifiedServer(req, res);
 });
 
-server.listen(3000, () => console.log('Server is listening on port 3000'));
+httpServer.listen(config.httpPort, () => console.log(`HTTP server is listening on port ${config.httpPort} in ${config.envName} mode`));
+
+const httpsServerOptions = {
+    key: fs.readFileSync('./https/key.pem'),
+    cert: fs.readFileSync('./https/cert.pem')
+};
+
+const httpsServer = https.createServer(httpsServerOptions, (req, res) => {
+    unifiedServer(req, res);
+});
+
+httpsServer.listen(config.httpsPort, () => console.log(`HTTPS server is listening on port ${config.httpsPort} in ${config.envName} mode`));
